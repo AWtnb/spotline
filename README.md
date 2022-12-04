a0ea0c128853508b0d86e3455389939b1aaf9cf4
awtnb <arikaebanataw@gmail.com>
2022-12-04 11:36:39 +0900

    update

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # spotline
 
-Personal customization of [L3v3L/vs-focus](https://github.com/L3v3L/vs-focus).
+got inspired by: [L3v3L/vs-focus](https://github.com/L3v3L/vs-focus).
 
 ## Features
 
-+ `spotline.apply` (ctrl+f8) blurs unselected lines.
-    + if this command is executed while other lines are already blurred, the blur will be removed.
-+ `spotline.reset` (ctrl+shift+f8) removes all blur.
++ `spotline.apply` ( <kbd>ctrl+f8</kbd> ) blurs unselected lines.
+    + if this command is executed while other lines are already blurred, the blur will be cleared.
++ `spotline.reset` ( <kbd>ctrl+shift+f8</kbd> ) clears all blur.
+
+## Currently known issues
+
++ Switching tabs resets the blur. In order to reapply, `spotline.apply` must be run again.
+
 
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,21 @@
 import * as vscode from "vscode";
 
-// https://github.com/L3v3L/vs-focus/blob/master/extension.js
-
 class Spotter {
-  deco: vscode.TextEditorDecorationType;
-  focusStart: number;
-  focusEnd: number;
+  readonly deco: vscode.TextEditorDecorationType;
+  focusStart: number = -1;
+  focusEnd: number = -1;
 
   constructor(opacity: number) {
     this.deco = vscode.window.createTextEditorDecorationType({
       opacity: `${opacity} !important`,
     });
+  }
+
+  private clrearDeco(editor: vscode.TextEditor) {
+    editor.setDecorations(this.deco, []);
+  }
+
+  resetPosition() {
     this.focusStart = -1;
     this.focusEnd = -1;
   }
@@ -24,25 +29,26 @@ class Spotter {
     const selTop = editor.selection.start.line;
     const selBottom = editor.selection.end.line;
 
-    editor.setDecorations(this.deco, []);
+    this.clrearDeco(editor);
     if (selTop == this.focusStart && selBottom == this.focusEnd) {
-      this.focusStart = -1;
-      this.focusEnd = -1;
+      this.resetPosition();
       return;
     }
 
     const docStart = new vscode.Position(0, 0);
     const docEnd = editor.document.lineAt(editor.document.lineCount - 1).rangeIncludingLineBreak.end;
-    const blurRange = [];
+    const blurTarget = [];
+
     if (selTop > 0) {
       const prevLineEnd = editor.document.lineAt(selTop - 1).rangeIncludingLineBreak.end;
-      blurRange.push(new vscode.Range(docStart, prevLineEnd));
+      blurTarget.push(new vscode.Range(docStart, prevLineEnd));
     }
     if (selBottom < editor.document.lineCount - 1) {
       const nextLineStart = editor.document.lineAt(selBottom + 1).range.start;
-      blurRange.push(new vscode.Range(nextLineStart, docEnd));
+      blurTarget.push(new vscode.Range(nextLineStart, docEnd));
     }
-    editor.setDecorations(this.deco, blurRange);
+
+    editor.setDecorations(this.deco, blurTarget);
 
     this.focusStart = selTop;
     this.focusEnd = selBottom;
@@ -54,19 +60,25 @@ class Spotter {
       console.log("no editor is opened.");
       return;
     }
-    editor.setDecorations(this.deco, []);
-    this.focusStart = -1;
-    this.focusEnd = -1;
+    this.clrearDeco(editor);
+    this.resetPosition();
   }
 }
 
-const config = vscode.workspace.getConfiguration("spotline");
-const opacity: number = config.get("opacity") || 0.6;
-const SPOTTER = new Spotter(opacity);
+const getOpacityConfig = ():number => {
+	const config = vscode.workspace.getConfiguration("spotline");
+	return config.get("opacity") || 0.4;
+}
+
+const SPOTTER = new Spotter(getOpacityConfig());
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(vscode.commands.registerCommand("spotline.apply", () => SPOTTER.spotlight()));
   context.subscriptions.push(vscode.commands.registerCommand("spotline.reset", () => SPOTTER.unSpotlight()));
 }
+
+vscode.window.onDidChangeActiveTextEditor(() => {
+  SPOTTER.resetPosition();
+});
 
 export function deactivate() {}
