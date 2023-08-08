63bcb671659b1eacc07d87e7275ce2fb0e104739
awtnb <arikaebanataw@gmail.com>
2023-08-08 22:59:55 +0900

    support highlight selction

--- src/spotter.ts
+++ src/spotter.ts
@@ -6,20 +6,48 @@ class EditorCursors {
     this._editor = editor;
   }
 
-  private getCursorLines(): number[] {
-    const lines: number[] = [];
-    this._editor.selections.forEach((sel) => {
-      const line = sel.active.line;
-      if (lines.includes(line)) {
-        return;
-      }
-      lines.push(line);
+  private toLineRanges(): vscode.Range[] {
+    return this._editor.selections.map((sel) => {
+      const start = new vscode.Position(sel.start.line, 0);
+      const end = new vscode.Position(sel.end.line, this._editor.document.lineAt(sel.end.line).text.length);
+      return new vscode.Range(start, end);
     });
-    return lines.sort((a: number, b: number): number => {
-      if (a < b) return -1;
-      if (b < a) return 1;
+  }
+
+  private getOrderedRanges(): vscode.Range[] {
+    return this.toLineRanges().sort((a: vscode.Range, b: vscode.Range): number => {
+      if (a.start.line < b.start.line) {
+        return -1;
+      }
+      if (b.start.line < a.start.line) {
+        return 1;
+      }
       return 0;
     });
+  }
+
+  /**
+   * When multiple cursors are on the same line (or one end of a selection is on the same line as another cursor), `toLineRanges()` creates overlapping Ranges.
+   * This method unites the overlapping Ranges with each other. The finally returned Ranges will not overlap at all.
+   */
+  private uniteRanges(): vscode.Range[] {
+    const cursorRanges = this.getOrderedRanges();
+    const ranges: vscode.Range[] = [];
+    for (let i = 0; i < cursorRanges.length; i++) {
+      const c = cursorRanges[i];
+      if (i < 1) {
+        ranges.push(c);
+        continue;
+      }
+      const last = ranges[ranges.length - 1];
+      if (last.intersection(c)) {
+        ranges.pop();
+        ranges.push(last.union(c));
+      } else {
+        ranges.push(c);
+      }
+    }
+    return ranges;
   }
 
   private getLineEnd(lineIdx: number): vscode.Position {
@@ -32,24 +60,24 @@ class EditorCursors {
 
   getInterCursorRanges(): vscode.Range[] {
     const ranges: vscode.Range[] = [];
-    const lines = this.getCursorLines();
-    const firstCursorLine = lines[0];
-    if (0 < firstCursorLine) {
+    const sels = this.uniteRanges();
+    const firstSelection = sels[0];
+    if (0 < firstSelection.start.line) {
       const bof = this.getLineStart(0);
-      const end = this.getLineStart(firstCursorLine);
+      const end = firstSelection.start;
       const range = new vscode.Range(bof, end);
       ranges.push(range);
     }
-    for (let i = 1; i < lines.length; i++) {
-      const start = this.getLineEnd(lines[i - 1]);
-      const end = this.getLineStart(lines[i]);
+    for (let i = 1; i < sels.length; i++) {
+      const start = sels[i - 1].end;
+      const end = sels[i].start;
       const range = new vscode.Range(start, end);
       ranges.push(range);
     }
     const maxLineIdx = this._editor.document.lineCount - 1;
-    const lastCursorLine = lines[lines.length - 1];
-    if (lastCursorLine < maxLineIdx) {
-      const start = this.getLineEnd(lastCursorLine);
+    const lastSelection = sels[sels.length - 1];
+    if (lastSelection.end.line < maxLineIdx) {
+      const start = lastSelection.end;
       const eof = this.getLineEnd(maxLineIdx);
       const range = new vscode.Range(start, eof);
       ranges.push(range);
